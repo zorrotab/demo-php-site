e70910191f44dea58b15ab8816dede5f50f460aa
Justin <zorrotab@gmail.com>
2025-10-15 20:14:38 +1100

    Update router script

--- index.php
+++ index.php
@@ -1,16 +1,7 @@
 <?php
 
-$request = trim(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH), '/');
+require_once 'router.php';
 
-if ($request === '') {
-    $request = 'home';
-}
+$route = new Router();
 
-$page = __DIR__ . '/pages/' . $request . '.php';
-
-if (file_exists($page)) {
-    require $page;
-} else {
-    http_response_code(404);
-    echo "404 Not Found";
-}
+$route->dispatch();
--- router.php
+++ router.php
@@ -1,12 +1,27 @@
 <?php
 
-// Serve static files directly (CSS, JS, images)
-if (php_sapi_name() === 'cli-server') {
-    $path = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
-    $file = __DIR__ . $path;
-    if (is_file($file)) {
-        return false;
+class Router {
+    private $routes = [];
+
+    public function __construct() {
+        $this->routes = [
+            'home' => 'pages/home.php',
+            'welcome' => 'pages/welcome.php',
+        ];
+    }
+
+    public function dispatch() {
+        $request = trim(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH), '/');
+        
+        if (empty($request)) {
+            $request = 'home';
+        }
+
+        if (array_key_exists($request, $this->routes)) {
+            require $this->routes[$request];
+        } else {
+            http_response_code(404);
+            echo "404 Not Found";
+        }
     }
 }
-
-require 'index.php';
