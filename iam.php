eadeaa76243eefb3bbc9b763e7d4a93a928fc56f
Justin <zorrotab@gmail.com>
2025-10-26 18:32:10 +1100

    Add user register logic

--- iam.php
+++ iam.php
@@ -4,6 +4,7 @@ $action = urlencode($_POST['action'] ?? '');
 $name = urlencode($_POST['name'] ?? '');
 $password = urlencode($_POST['password'] ?? '');
 
+# Open database
 class MyDB extends SQLite3 {
       function __construct() {
          $this->open('data.db');
@@ -16,12 +17,52 @@ class MyDB extends SQLite3 {
 }
 
 
-echo $action;
-
+# Check login credentials
 if ($action === 'Login') {
     header("Location: /welcome?name=$name&password=$password");
+
+# Check if user already exists
 } elseif ($action === 'Register') {
-        echo "Registering user";
+    $stmt = $db->prepare('SELECT NAME FROM USERS WHERE NAME = :name');
+    $stmt->bindValue(':name', $name, SQLITE3_TEXT);
+    $ret = $stmt->execute();
+    $userExists = false;
+    while ($row = $ret->fetchArray(SQLITE3_ASSOC)) {
+        if ($row['NAME'] === $name) {
+            $userExists = true;
+            break;
+        }
+    }
+
+    # Inform that user already exists
+    if ($userExists) {
+        echo "user already exists\n";
+
+        # TO DO
+        # Redirect to home.php
+        # Add box for return result
+
+    # Create new user
+    } else {
+        # Get unique id
+        $stmt = $db->prepare('SELECT COUNT(*) as count FROM USERS');
+        $ret = $stmt->execute();
+        $row = $ret->fetchArray();
+        $id = $row['count'] + 1;
+
+        # Add new user to table
+        $stmt = $db->prepare('INSERT INTO USERS (ID,NAME,PASSWORD) VALUES (:id, :name, :password)');
+        $stmt->bindValue(':id', $id, SQLITE3_TEXT);
+        $stmt->bindValue(':name', $name, SQLITE3_TEXT);
+        $stmt->bindValue(':password', $password, SQLITE3_TEXT);
+        $ret = $stmt->execute();
+        if(!$ret) {
+            echo $db->lastErrorMsg();
+        } else {
+            echo "user created\n";
+        }
+        $db->close();
+    }
 
 } else {
     http_response_code(404);
